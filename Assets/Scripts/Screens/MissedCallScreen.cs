5856c4ae26dd7651f329d0866121e26813b9381f
woutervmertens <woutervmertens@gmail.com>
2021-03-10 15:02:12 +0100

    Music and call screen scripts

---
+++ Assets/Scripts/Screens/MissedCallScreen.cs
@@ -0,0 +1,17 @@
+﻿using DefaultNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissedCallScreen : MenuScreen
+{
+    public override void OnTriggered(int n)
+    {
+        base.OnTriggered(n);
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+    }
+}
